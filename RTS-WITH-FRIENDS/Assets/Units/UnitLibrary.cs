298cd303fcd4d1168e55af27cab05fbf8900efd9
Victor Forsgren <victor@forsgrens.nu>
2017-12-11 21:57:30 +0100

    ISelectable and Library restructuring

--- RTS-WITH-FRIENDS/Assets/Units/UnitLibrary.cs
+++ RTS-WITH-FRIENDS/Assets/Units/UnitLibrary.cs
@@ -3,6 +3,5 @@ using System.Collections.Generic;
 using UnityEngine;
 using System.Linq;
 [System.Serializable]
-public class UnitLibrary{
-	public List<Unit> Units;
+public class UnitLibrary : Library<Unit> {
 }
