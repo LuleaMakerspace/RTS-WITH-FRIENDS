7f072d34488867d7634ee0027a7889e3fa35a4fa
Victor Forsgren <victor@forsgrens.nu>
2017-12-12 20:07:06 +0100

    Removed annoying comments

--- RTS-WITH-FRIENDS/Assets/GameController.cs
+++ RTS-WITH-FRIENDS/Assets/GameController.cs
@@ -2,19 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 public class GameController : MonoBehaviour {
-
-	// This is where magic happens
-	// > No
-	// > YEs
-	// > Is Victor delusional?
-	// > Proably
-	// > Cool
-
 	public Factory TestFactory;
 	public UnitLibrary UnitLibrary;
 	public static GameController Instance;
 
-	// Use this for initialization
 	void Awake () {
 		if (Instance == null) {
 			Instance = this;
