b6de6e24a402f6504bd62d77c22d312758b2ac7e
Victor Forsgren <victor@forsgrens.nu>
2017-12-12 18:07:09 +0100

    Resources

---
+++ RTS-WITH-FRIENDS/Assets/Resource/Resource.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Resource {
+	public int Amount {
+		get; set;
+	}
+	public ResourceType ResourceType {
+		get; private set;
+	}
+	public Resource(ResourceType resourceType, int amount = 0) {
+		this.ResourceType = resourceType;
+		this.Amount = amount;
+	}
+}
--- RTS-WITH-FRIENDS/Assets/Units/Unit.cs
+++ RTS-WITH-FRIENDS/Assets/Units/Unit.cs
@@ -5,4 +5,20 @@ using UnityEngine;
 public class Unit : MonoBehaviour {
   public float Speed;
   public UnitType UnitType;
+  public Vector2 Target {
+    get; set;
+  }
+
+  public virtual void Update() {
+
+  }
+
+  public virtual void Awake() {
+    print(UnitType + " awoken");
+  }
+
+  public virtual void Start() {
+    print(UnitType + " spawned");
+  }
+
 }
