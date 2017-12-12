0ee0e40f4f4577ad85fe343d32a1bd2e03e5e713
Victor Forsgren <victor@forsgrens.nu>
2017-12-12 17:14:18 +0100

    Important fix

--- RTS-WITH-FRIENDS/Assets/Units/Tank/TankBlueprint.cs
+++ RTS-WITH-FRIENDS/Assets/Units/Tank/TankBlueprint.cs
@@ -24,7 +24,7 @@ public class TankBlueprint : IBlueprint
   {
     get
     {
-      return GameController.Instance.UnitLibrary.Entries.First(u => u.Id == "Tank");
+      return GameController.Instance.UnitLibrary.Entries.First(u => u.UnitType == UnitType.Tank);
     }
   }
 }
