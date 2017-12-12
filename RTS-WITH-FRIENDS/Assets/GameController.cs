ceaf5f1aee6f9e9c13be73d202518a2c7eebc6c7
Victor Forsgren <victor@forsgrens.nu>
2017-12-12 19:45:33 +0100

    Moved code to a selection manager

--- RTS-WITH-FRIENDS/Assets/GameController.cs
+++ RTS-WITH-FRIENDS/Assets/GameController.cs
@@ -14,11 +14,6 @@ public class GameController : MonoBehaviour {
 	public UnitLibrary UnitLibrary;
 	public static GameController Instance;
 
-	private List<ISelectable> selectedObjects = new List<ISelectable>();
-
-	private Vector2 startDrag;
-	private Vector2 endDrag;
-
 	// Use this for initialization
 	void Awake () {
 		if (Instance == null) {
@@ -29,42 +24,9 @@ public class GameController : MonoBehaviour {
 		}
 	}
 
-	// Update is called once per frame
 	void Update () {
 		if (Input.GetKeyDown(KeyCode.Q)) {
 			TestFactory.PushUnit(new TankBlueprint());
 		}
-		// Basic select prototype
-		if (Input.GetMouseButtonDown(0)) {
-			startDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-		}
-		if (Input.GetMouseButtonUp(0)) {
-			endDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-			if (Vector2.Distance(startDrag, endDrag) < 0.3) {
-				RaycastHit2D hit = Physics2D.Raycast(endDrag, -Vector2.up);
-				if (hit.collider != null) {	
-					if (hit.transform.GetComponent<ISelectable>() != null) {
-						SelectObjects(new List<ISelectable>() {hit.transform.GetComponent<ISelectable>()});
-					}
-				}
-				else {
-					RemoveSelection();
-				}
-			}
-			else {
-
-			}
-		}
-		if (Input.GetMouseButtonDown(1)) {
-			this.selectedObjects.ForEach(selectedObject => selectedObject.OnDirectionsRecived(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
-		}
-	}
-	private void RemoveSelection() {
-		this.selectedObjects.ForEach(selectedObject => selectedObject.OnUnSelect());
-		this.selectedObjects = new List<ISelectable>();
-	}
-	private void SelectObjects(List<ISelectable> objects) {
-		this.selectedObjects = objects;
-		this.selectedObjects.ForEach(selectedObject => selectedObject.OnSelect());
 	}
 }
---
+++ RTS-WITH-FRIENDS/Assets/SelectionController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionController : MonoBehaviour {
+	private List<ISelectable> selectedObjects = new List<ISelectable>();
+	private Vector2 startDrag;
+	private Vector2 endDrag;
+	void Update() {
+		if (Input.GetMouseButtonDown(0)) {
+			startDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		}
+		if (Input.GetMouseButtonUp(0)) {
+			endDrag = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+			if (Vector2.Distance(startDrag, endDrag) < 0.3) {
+				RaycastHit2D hit = Physics2D.Raycast(endDrag, -Vector2.up);
+				if (hit.collider != null) {	
+					if (hit.transform.GetComponent<ISelectable>() != null) {
+						SelectObjects(new List<ISelectable>() {hit.transform.GetComponent<ISelectable>()});
+					}
+				}
+				else {
+					RemoveSelection();
+				}
+			}
+			else {
+
+			}
+		}
+		if (Input.GetMouseButtonDown(1)) {
+			this.selectedObjects.ForEach(selectedObject => selectedObject.OnDirectionsRecived(Camera.main.ScreenToWorldPoint(Input.mousePosition)));
+		}
+	}
+	private void RemoveSelection() {
+		this.selectedObjects.ForEach(selectedObject => selectedObject.OnUnSelect());
+		this.selectedObjects = new List<ISelectable>();
+	}
+	private void SelectObjects(List<ISelectable> objects) {
+		this.selectedObjects = objects;
+		this.selectedObjects.ForEach(selectedObject => selectedObject.OnSelect());
+	}
+}
