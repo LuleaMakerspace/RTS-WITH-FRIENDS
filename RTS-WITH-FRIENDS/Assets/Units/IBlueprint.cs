5ce770631349bd2cb43da825b730c905002494ac
Victor Forsgren <victor@forsgrens.nu>
2017-12-12 17:06:53 +0100

    Nice comment

--- RTS-WITH-FRIENDS/Assets/Units/IBlueprint.cs
+++ RTS-WITH-FRIENDS/Assets/Units/IBlueprint.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+/*
+	Vad som behövs för att kunna bygga en Unit
+ */
 public interface IBlueprint {
 	int RequiredEnergy {
 		get;
