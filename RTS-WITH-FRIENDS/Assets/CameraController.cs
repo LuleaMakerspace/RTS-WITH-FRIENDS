ef21930991e835034a58dd1f6dd91954af4a3189
Victor Forsgren <victor@forsgrens.nu>
2017-12-13 15:17:16 +0100

    Zooming

--- RTS-WITH-FRIENDS/Assets/CameraController.cs
+++ RTS-WITH-FRIENDS/Assets/CameraController.cs
@@ -6,10 +6,19 @@ public class CameraController : MonoBehaviour {
 	private Vector2 dragStart;
 	public float MoveSpeed;
 
+	public float MaxZoom;
+	public float MinZoom;
+	public float ZoomSpeed;
+
 	void Start () {
 		
 	}
 	void Update () {
 		transform.position += (Vector3)new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")) * MoveSpeed * Time.deltaTime;
+		if (Input.GetAxis("Mouse ScrollWheel") > 0 && Camera.main.orthographicSize > MinZoom) {
+			Camera.main.orthographicSize -= ZoomSpeed;
+		} else if (Input.GetAxis("Mouse ScrollWheel") < 0 && Camera.main.orthographicSize < MaxZoom) {
+			Camera.main.orthographicSize += ZoomSpeed;
+		}
 	}
 }
