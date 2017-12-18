a9a7a221b70dba1a80363e8c5d2b9bd6c23af9c5
Olov Johansson <johansson.olov@gmail.com>
2017-12-18 19:35:55 +0100

    asdasd

---
+++ RTS-WITH-FRIENDS/Assets/Pathfinding/Pathfinding.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+
+public class Pathfinding : MonoBehaviour, IDisposable {
+	 Grid grid;
+
+		void Awake()
+		{
+			grid = GetComponent<Grid>();
+		}
+
+        public void FindPathPriority(Vector2 startPos, Vector2 targetPos, int id, Action<List<Vector2>, int> callback)
+        {
+            var startNode = grid.NodeFromWorldPoint(startPos);
+            var targetNode = grid.NodeFromWorldPoint(targetPos);
+
+            var openSet = new PriorityQueue<Node>(grid.MaxSize);
+            var closedSet = new HashSet<Node>();
+            openSet.Enqueue(startNode);
+            var hasPath = false;
+            while (openSet.Count > 0)
+            {
+                var currentNode = openSet.Dequeue();
+                closedSet.Add(currentNode);
+
+                if (currentNode == targetNode)
+                {
+                    hasPath = true;
+                    break;
+                }
+
+                foreach (var neighbour in grid.GetNeighbours(currentNode))
+                {
+                    if (!neighbour.walkable || closedSet.Contains(neighbour))
+                    {
+                        continue;
+                    }
+
+                    int newMovementCostToNeighbour = currentNode.gCost + GetDistance(currentNode, neighbour);                    
+
+                    if (newMovementCostToNeighbour < neighbour.gCost || !openSet.Contains(neighbour))
+                    {
+                        neighbour.gCost = newMovementCostToNeighbour;
+                        neighbour.hCost = GetDistance(neighbour, targetNode) + newMovementCostToNeighbour;
+                        neighbour.parent = currentNode;
+
+                        if (!openSet.Contains(neighbour))
+                            openSet.Enqueue(neighbour);
+                        else
+                        {
+                            openSet.UpdateItem(neighbour);
+                        }
+                    }
+                }
+            }
+
+            if (hasPath)
+                callback(RetracePath(startNode, targetNode), id);
+            else
+                callback(null, id);
+        }
+
+        List<Vector2> RetracePath(Node startNode, Node endNode)
+        {
+            List<Node> path = new List<Node>();
+            Node currentNode = endNode;
+
+            while (currentNode != startNode)
+            {
+                path.Add(currentNode);
+                currentNode = currentNode.parent;
+            }
+            path.Reverse();
+            return path.Select(n => n.worldPosition).ToList();
+        }
+
+        int GetDistance(Node nodeA, Node nodeB)
+        {
+            int dstX = Math.Abs(nodeA.gridX - nodeB.gridX);
+            int dstY = Math.Abs(nodeA.gridY - nodeB.gridY);
+
+            if (dstX > dstY)
+                return 14 * dstY + 10 * (dstX - dstY);
+            return 14 * dstX + 10 * (dstY - dstX);
+        }
+
+        public void Dispose()
+        {           
+        }
+}
---
+++ RTS-WITH-FRIENDS/Assets/Pathfinding/PriorityQueue.cs
@@ -0,0 +1,234 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+ public class PriorityQueue<T>
+       where T : class, INode<T>
+    {
+        private int _numNodes;
+        private T[] _nodes;
+
+        public PriorityQueue(int maxNodes)
+        {
+            _numNodes = 0;
+            _nodes = new T[maxNodes + 1];
+        }
+
+        public int Count
+        {
+            get
+            {
+                return _numNodes;
+            }
+        }
+
+        public bool Contains(T node)
+        {
+            return (_nodes[node.QueueIndex] == node);
+        }
+
+        public void Enqueue(T node)
+        {            
+            _numNodes++;
+            _nodes[_numNodes] = node;
+            node.QueueIndex = _numNodes;
+            CascadeUp(node);
+        }
+
+        private void CascadeUp(T node)
+        {
+            int parent;
+            if (node.QueueIndex > 1)
+            {
+                parent = node.QueueIndex >> 1;
+                T parentNode = _nodes[parent];
+                if (HasHigherOrEqualPriority(parentNode, node))
+                    return;
+
+                _nodes[node.QueueIndex] = parentNode;
+                parentNode.QueueIndex = node.QueueIndex;
+
+                node.QueueIndex = parent;
+            }
+            else
+            {
+                return;
+            }
+            while (parent > 1)
+            {
+                parent >>= 1;
+                T parentNode = _nodes[parent];
+                if (HasHigherOrEqualPriority(parentNode, node))
+                    break;
+
+                _nodes[node.QueueIndex] = parentNode;
+                parentNode.QueueIndex = node.QueueIndex;
+
+                node.QueueIndex = parent;
+            }
+            _nodes[node.QueueIndex] = node;
+        }
+
+        private void CascadeDown(T node)
+        {
+            int finalQueueIndex = node.QueueIndex;
+            int childLeftIndex = 2 * finalQueueIndex;
+
+            if (childLeftIndex > _numNodes)
+            {
+                return;
+            }
+
+            int childRightIndex = childLeftIndex + 1;
+            T childLeft = _nodes[childLeftIndex];
+            if (HasHigherPriority(childLeft, node))
+            {
+                if (childRightIndex > _numNodes)
+                {
+                    node.QueueIndex = childLeftIndex;
+                    childLeft.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = childLeft;
+                    _nodes[childLeftIndex] = node;
+                    return;
+                }
+                T childRight = _nodes[childRightIndex];
+                if (HasHigherPriority(childLeft, childRight))
+                {
+                    childLeft.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = childLeft;
+                    finalQueueIndex = childLeftIndex;
+                }
+                else
+                {
+                    childRight.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = childRight;
+                    finalQueueIndex = childRightIndex;
+                }
+            }
+            else if (childRightIndex > _numNodes)
+            {
+                return;
+            }
+            else
+            {
+                T childRight = _nodes[childRightIndex];
+                if (HasHigherPriority(childRight, node))
+                {
+                    childRight.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = childRight;
+                    finalQueueIndex = childRightIndex;
+                }
+                else
+                {
+                    return;
+                }
+            }
+
+            while (true)
+            {
+                childLeftIndex = 2 * finalQueueIndex;
+
+                if (childLeftIndex > _numNodes)
+                {
+                    node.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = node;
+                    break;
+                }
+
+                childRightIndex = childLeftIndex + 1;
+                childLeft = _nodes[childLeftIndex];
+                if (HasHigherPriority(childLeft, node))
+                {
+                    if (childRightIndex > _numNodes)
+                    {
+                        node.QueueIndex = childLeftIndex;
+                        childLeft.QueueIndex = finalQueueIndex;
+                        _nodes[finalQueueIndex] = childLeft;
+                        _nodes[childLeftIndex] = node;
+                        break;
+                    }
+                    T childRight = _nodes[childRightIndex];
+                    if (HasHigherPriority(childLeft, childRight))
+                    {
+                        childLeft.QueueIndex = finalQueueIndex;
+                        _nodes[finalQueueIndex] = childLeft;
+                        finalQueueIndex = childLeftIndex;
+                    }
+                    else
+                    {
+                        childRight.QueueIndex = finalQueueIndex;
+                        _nodes[finalQueueIndex] = childRight;
+                        finalQueueIndex = childRightIndex;
+                    }
+                }
+                else if (childRightIndex > _numNodes)
+                {
+                    node.QueueIndex = finalQueueIndex;
+                    _nodes[finalQueueIndex] = node;
+                    break;
+                }
+                else
+                {
+                    T childRight = _nodes[childRightIndex];
+                    if (HasHigherPriority(childRight, node))
+                    {
+                        childRight.QueueIndex = finalQueueIndex;
+                        _nodes[finalQueueIndex] = childRight;
+                        finalQueueIndex = childRightIndex;
+                    }
+                    else
+                    {
+                        node.QueueIndex = finalQueueIndex;
+                        _nodes[finalQueueIndex] = node;
+                        break;
+                    }
+                }
+            }
+        }
+
+        private bool HasHigherPriority(T higher, T lower)
+        {
+            return (higher.Priority < lower.Priority);
+        }
+
+        private bool HasHigherOrEqualPriority(T higher, T lower)
+        {
+            return (higher.Priority <= lower.Priority);
+        }
+
+        public T Dequeue()
+        {
+            T returnMe = _nodes[1];
+
+            if (_numNodes == 1)
+            {
+                _nodes[1] = null;
+                _numNodes = 0;
+                return returnMe;
+            }
+
+            T formerLastNode = _nodes[_numNodes];
+            _nodes[1] = formerLastNode;
+            formerLastNode.QueueIndex = 1;
+            _nodes[_numNodes] = null;
+            _numNodes--;
+
+            CascadeDown(formerLastNode);
+            return returnMe;
+        }
+
+        public void UpdateItem(T node)
+        {
+            int parentIndex = node.QueueIndex >> 1;
+
+            if (parentIndex > 0 && HasHigherPriority(node, _nodes[parentIndex]))
+            {
+                CascadeUp(node);
+            }
+            else
+            {
+                CascadeDown(node);
+            }
+        }
+    }
+
