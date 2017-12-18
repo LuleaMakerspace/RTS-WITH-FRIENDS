0a37a63c714063ce345014df016b9d8b2c2da985
Olov Johansson <johansson.olov@gmail.com>
2017-12-18 19:17:48 +0100

    First A* test

---
+++ RTS-WITH-FRIENDS/Assets/Pathfinding/Grid.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Grid : MonoBehaviour, IDisposable {
+		public LayerMask UnwalkableMask; 
+		public Vector2 Banan;
+        public float nodeRadius;
+        Node[,] grid;
+        public int MaxSize { get; private set;}
+        float nodeDiameter;
+        int gridSizeX, gridSizeY;
+        
+		void Awake()
+        {
+            nodeDiameter = nodeRadius * 2;
+            gridSizeX = Mathf.RoundToInt(Banan.x / nodeDiameter);
+            gridSizeY = Mathf.RoundToInt(Banan.y / nodeDiameter);
+            MaxSize = gridSizeX * gridSizeY;
+
+            CreateGrid();
+        }
+
+        void CreateGrid()
+        {
+            grid = new Node[gridSizeX, gridSizeY];
+
+			Vector2 worldBottomLeft =  new Vector2(transform.position.x, transform.position.y) - Vector2.right * Banan.x / 2 + Vector2.down * Banan.y/2; 
+
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                for (int y = 0; y < gridSizeY; y++)
+                {
+                    Vector2 worldPoint = worldBottomLeft + new Vector2((x * nodeDiameter + nodeRadius), (y * nodeDiameter + nodeRadius));
+  					bool walkable = !(Physics2D.OverlapCircle(worldPoint, nodeRadius, UnwalkableMask, -2, 10)); 
+                    grid[x, y] = new Node(walkable, worldPoint, x, y);
+                }
+            }
+        }
+
+        public List<Node> GetNeighbours(Node node)
+        {
+            List<Node> neighbours = new List<Node>();
+
+            for (int x = -1; x <= 1; x++)
+            {
+                for (int y = -1; y <= 1; y++)
+                {
+                    if (x == 0 && y == 0)
+                        continue;
+
+                    int checkX = node.gridX + x;
+                    int checkY = node.gridY + y;
+
+                    if (checkX >= 0 && checkX < gridSizeX && checkY >= 0 && checkY < gridSizeY)
+                    {
+                        neighbours.Add(grid[checkX, checkY]);
+                    }
+                }
+            }
+
+            return neighbours;
+        }        
+
+        public Node NodeFromWorldPoint(Vector2 worldPosition)
+        {
+            int x = Mathf.RoundToInt((gridSizeX - 1) * ((worldPosition.x) / Banan.x));
+            int y = Mathf.RoundToInt((gridSizeY - 1) * ((worldPosition.y) / Banan.y));
+            return grid[x, y];
+        }
+
+        public void Dispose()
+        {
+          
+        } 
+
+		void OnDrawGizmos()  
+		{ 
+			Gizmos.color = Color.green; 
+			Gizmos.DrawWireCube(transform.position, new Vector3(Banan.x, Banan.y, 1)); 
+			print(grid);
+			if(grid != null)  
+			{ 
+				Debug.Log("Inne"); 
+				int i = 0; 
+				foreach(var n in grid)  
+				{ 
+					Gizmos.color = n.walkable ? Color.blue : Color.red; 
+					var v = new Vector3(n.worldPosition.x, n.worldPosition.y, 1); 
+					print(i); 
+					i = i++; 
+					Gizmos.DrawWireCube(v, Vector3.one * (nodeDiameter - .1f));   
+				} 
+			} 
+		} 
+	}
---
+++ RTS-WITH-FRIENDS/Assets/Pathfinding/Node.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : INode<Node> {
+
+	 public bool walkable;
+        public Vector2 worldPosition;
+        public int gridX;
+        public int gridY;
+
+        public int gCost;
+        public int hCost;
+        public Node parent;
+
+        public Node(bool _walkable, Vector2 _worldPos, int _gridX, int _gridY)
+        {
+            walkable = _walkable;
+            worldPosition = _worldPos;
+            gridX = _gridX;
+            gridY = _gridY;
+        }
+
+        public int fCost
+        {
+            get
+            {
+                return gCost + hCost;
+            }
+        }
+
+        public int QueueIndex { get; set; }
+
+        public int Priority
+        {
+            get
+            {
+                return fCost;
+            }
+        }
+
+        public int CompareTo(Node other)
+        {
+            int compare = fCost.CompareTo(other.fCost);
+
+            if (compare == 0)
+            {
+                compare = hCost.CompareTo(other.hCost);
+            }
+
+            return -compare;
+        }
+}
+
+public interface INode<T>
+{
+	int Priority { get; }
+	int QueueIndex { get; set; }
+}
